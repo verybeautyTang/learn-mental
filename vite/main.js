e52a8232d2a233241d8803d72dd229a8c9ab194a
verybeautytang <353332175@qq.com>
2025-12-09 20:11:21 +0800

    feat: add vite demo -basic

---
+++ vite/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './src/views/App.vue'
+const app = createApp(App)
+app.mount('#app')
---
+++ vite/vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// 情景配置
+export default defineConfig({
+  plugins: [vue()],
+})
